5d6ceffa8e78591b00b4383519b2b486e56354f8
Emine Mutlu <emine.mutlu85@gmail.com>
2022-10-03 23:34:40 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <router-link
+    class="menu-link"
+    active-class="active"
+    to="/dummydata"
+    >
+    <span class="menu-title text-muted" style="font-size: 3rem; margin-left: 20px;">DummyData</span>
+  </router-link>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
